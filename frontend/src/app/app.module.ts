eb8879988ef944bc67fe9cc9d53b5a2426d7afb8
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-15 16:56:14 -0300

    api-frontend integration and product view

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductService } from './services/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [ProductService], // service global dentro da aplicação
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
